0c5b685aa0fa5ea30fb63ffd0d055ed3e5cb94c9
kapilchoudhary <kchoudhary006@gmail.com>
2022-07-26 12:43:06 +0530

    Added create method for product

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -5,11 +5,35 @@ class Api::ProductsController < ApplicationController
     render json: @products, :include => {:properties => {}}
   end
 
-  def create    
+  def create
+    @product = Product.new(product_params)
+
+    property_params.each do |params|
+      @product.properties.create(params)
+    end
+    render json: {status: true, message: 'Product created successfully'}
   end
 
   def search
     @products = params[:name].present? ? Product.where("name LIKE ?", "%#{params[:name]}%") : []
     render json: @products, :include => {:properties => {}}
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(
+      :name,
+      :upc,
+      :available_on
+    )
+  end
+
+  def property_params
+    params.permit(properties: [
+      :name,
+      :value
+     ])
+     .require(:properties)
   end
 end
--- app/models/property.rb
+++ app/models/property.rb
@@ -1,5 +1,5 @@
 class Property < ApplicationRecord
   belongs_to :product
 
-  validates :name, length: { maximum: 255 }, uniqueness: {scope: :product}
+  validates :name, length: { maximum: 255 }, uniqueness: {scope: :product}, presence: :true
 end
